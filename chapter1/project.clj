e352d750f83dc12ea79dac3f5f2d327766405f5f
Giovanni Martina <gio@popado.co>
2017-05-21 09:36:22 -0300

    Update project file

--- chapter1/project.clj
+++ chapter1/project.clj
@@ -1,6 +1,7 @@
 (defproject chapter1 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]]}})
